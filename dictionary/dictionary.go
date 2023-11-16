4b00907896fd6671d93ea468b9c8113820e0bdb6
MouadMB <58629183+DrackMB@users.noreply.github.com>
2023-11-16 12:28:54 +0100

    first commit

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,11 +1,14 @@
 package dictionary
 
+import "fmt"
+
 type Entry struct {
+	Def string
 }
 
 func (e Entry) String() string {
 
-	return ""
+	return e.Def
 }
 
 type Dictionary struct {
@@ -14,23 +17,32 @@ type Dictionary struct {
 
 func New() *Dictionary {
 
-	return nil
+	return &Dictionary{entries: make(map[string]Entry)}
 }
 
 func (d *Dictionary) Add(word string, definition string) {
-
+	entry := Entry{Def: definition}
+	d.entries[word] = entry
 }
 
 func (d *Dictionary) Get(word string) (Entry, error) {
 
-	return Entry{}, nil
+	entry, exists := d.entries[word]
+	if !exists {
+		return Entry{}, fmt.Errorf("Word not found: %s", word)
+	}
+	return entry, nil
 }
 
 func (d *Dictionary) Remove(word string) {
-
+	delete(d.entries, word)
 }
 
 func (d *Dictionary) List() ([]string, map[string]Entry) {
 
-	return []string{}, d.entries
+	words := make([]string, 0, len(d.entries))
+	for word := range d.entries {
+		words = append(words, word)
+	}
+	return words, d.entries
 }
--- main.go
+++ main.go
@@ -2,26 +2,89 @@ package main
 
 import (
 	"bufio"
-
-	"dictionary/dictionary"
+	"estiam/dictionary"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	// bufio.NewReader(os.Stdin)
+	d := dictionary.New()
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Enter command (add/define/remove/list/exit): ")
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error:", err)
+			continue
+		}
+
+		command = strings.TrimSpace(command)
+
+		switch command {
+		case "add":
+			actionAdd(d, reader)
+		case "define":
+			actionDefine(d, reader)
+		case "remove":
+			actionRemove(d, reader)
+		case "list":
+			actionList(d)
+		case "exit":
+			fmt.Println("Exiting program.")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid command. Try again.")
+		}
+	}
 }
 
 func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter word: ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
+	fmt.Print("Enter definition: ")
+	definition, _ := reader.ReadString('\n')
+	definition = strings.TrimSpace(definition)
+
+	d.Add(word, definition)
+	fmt.Println("Word added successfully.")
 }
 
 func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter word to define: ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
+	entry, err := d.Get(word)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Data: %s\n", entry.String())
 }
 
 func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
+	fmt.Print("Enter word to remove: ")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
+	d.Remove(word)
+	fmt.Println("Word removed successfully.")
 }
 
 func actionList(d *dictionary.Dictionary) {
+	words, entries := d.List()
 
+	fmt.Println("Words:")
+	for _, word := range words {
+		fmt.Println(word)
+	}
+
+	fmt.Println("\nDictionary:")
+	for word, entry := range entries {
+		fmt.Printf("%s: %s\n", word, entry.String())
+	}
 }
